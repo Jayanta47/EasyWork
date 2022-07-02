522b2f3b376dbd0d29945e33e9938d51774feae6
Jayanta Sadhu <jayantasadhu4557@gmail.com>
2022-07-02 14:30:06 +0600

    Dependency and subtask models created

--- projectAndTasks/models.py
+++ projectAndTasks/models.py
@@ -1,6 +1,8 @@
+from turtle import title
 from django.db import models
 from requests import delete
 from ..userMgmt.models import Designation, User
+from ..costEstimation.models import FuncCategory
 # Create your models here.
 
 
@@ -68,4 +70,88 @@ class User_Project_Map(models.Model):
         null = False,
         default=None,
     )
+
+class Task(models.Model):
+
+    STATUS_LIST = [
+        ("C", "completed"),
+        ("O", "ongoing"),
+        ("P", "postponed"),
+        ("U", "not started"),
+    ]
+
+    id = models.AutoField(
+        primary_key=True 
+    )
+
+    project_id = models.ForeignKey(
+        on_delete=models.CASCADE
+    )
+
+    title = models.CharField(
+        max_length=200,
+        default="title",
+        null=True,
+    )
+
+    creation_time = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    start_time = models.DateField(
+        default=None,
+        null=True,
+    )
+
+    end_time = models.DateField(
+        default=None,
+        null=True 
+    )
+
+    slack_time = models.IntegerField(
+        default=0,
+        null=True 
+    )
+
+    status = models.CharField(
+        max_length=2,
+        choices=STATUS_LIST 
+    )
+
+    category_id = models.ForeignKey(
+        FuncCategory,
+        on_delete=models.SET_NULL,
+        default=None,
+        null=True 
+    )
+
+
+class TaskHierarchy (models.Model):
+    id = models.AutoField(
+        primary_key = True 
+    )
+
+    parent_task_id = models.ForeignKey(
+        Task,
+        on_delete=models.CASCADE
+    )
+
+    child_task_id = models.ForeignKey(
+        Task,
+        null=False,
+        on_delete=models.CASCADE
+    )
+
+
+class Dependency (models.Model):
+    id = models.AutoField(
+        primary_key=True 
+    )  
+
+    dependent_on_task_id = models.ForeignKey(
+        Task,
+        
+    )
+
+
 
--- taskMgmt/models.py
+++ taskMgmt/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from ..projectAndTasks.models import Task
 
 # Create your models here.
+
+class Dependency (models.Model):
+    id = models.AutoField(
+        primary_key=True 
+    )  
+
+    dependent_on_task = models.ForeignKey(
+        Task,
+        on_delete=models.CASCADE
+    )
+
+    parent_task = models.ForeignKey(
+        Task, 
+        on_delete=models.CASCADE
+    )
