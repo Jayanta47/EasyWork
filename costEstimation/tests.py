0fc7eabe37e3cd0b1c3814f6e04c80b9aa24fb02
Jayanta Sadhu <jayantasadhu4557@gmail.com>
2022-08-21 16:50:43 +0600

    added tasktimemap and related functions for cost-month graph

--- costEstimation/tests.py
+++ costEstimation/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
 # Create your tests here.
+from costEstimation.utils import cost_month_graph
+
+print(cost_month_graph)
--- costEstimation/utils.py
+++ costEstimation/utils.py
@@ -1,3 +1,7 @@
+from datetime import timedelta
+from math import inf
+from taskMgmt.utils import totalDaysInCategory, getCategoryTimeMap, timeOfCategoryInRange
+from .models import FuncCategory
 
 def estimateEffort(loc, task_level, months):
     if task_level == "easy":
@@ -15,3 +19,43 @@ def estimateEffort(loc, task_level, months):
         B = 0.39
 
     return (loc/p)**3 * B * months**-4
+
+
+def cost_month_graph():
+    all_categories = FuncCategory.objects.all().values()
+
+    category_props = {}
+
+    start_date = inf
+    end_date = inf 
+
+    for category in all_categories:
+        d = {
+            "title": category["title"],
+            "days_traversed": 0,
+            "total_days": totalDaysInCategory(category_id=category["id"]),
+            "type": "normal",
+            "category_time_map": getCategoryTimeMap(category_id=category["id"])
+        }
+
+        if start_date == inf:
+            start_date = d["category_time_map"][0][0]
+            end_date = d["category_time_map"][0][1]
+        else:
+            start_date = min(start_date, d["category_time_map"][0][0])
+            end_date = max(end_date, d["category_time_map"][0][1])
+
+        category_props[str(category["id"])] = d
+        
+
+    print(category_props)
+    print(start_date, end_date)
+    
+    time_slots = []
+    current_date = start_date
+    while current_date <= end_date:
+        time_slots.append((current_date, current_date + timedelta(days=9)))
+        current_date+=timedelta(days=10)
+    print(time_slots)
+    # for props in category_props:
+    #     start_date = min()
