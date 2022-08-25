24074b380a3a7fe1b64e592b0d953dc6b0f55725
Jayanta Sadhu <jayantasadhu4557@gmail.com>
2022-08-25 17:16:33 +0600

    dependency graph addition function done

--- taskMgmt/utils.py
+++ taskMgmt/utils.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timedelta, date
 from math import inf
-from unicodedata import category
 from costEstimation.models import FuncCategory
 from projectAndTasks.models import Project, Task, TaskHierarchy, User_Project_Map, Project_Category_Map
 from projectAndTasks.serializers import TaskSerializer
@@ -8,8 +7,15 @@ from taskMgmt.models import Dependency, User_Task_Map
 from userMgmt.models import User, Designation
 from costEstimation.models import FuncCategory
 
-from django.utils import timezone
 from django.db.models import Q
+
+import numpy as np
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+import random
+import string
+from easywork_be.settings import MEDIA_ROOT
 
 
 def getTasksList(project_id):
@@ -55,6 +61,56 @@ def getPredecessorTaskList(task_id_list):
             dependency_list.append(t)
     return dependency_list
 
+
+# get the duration of a task given its id 
+def getTaskDuration(task_id):
+    task = Task.objects.filter(id=task_id).values("start_time", "end_time").first()
+    return max(0, (task["end_time"] - task["start_time"]).days)
+
+def getTaskTitle(task_id):
+    task = Task.objects.filter(id=task_id).values("title").first()
+    return task["title"]
+
+def getPredGraphList(task_id_list):
+    pred_list = []
+    ac_list = []
+    duration_list = []
+    sorted(task_id_list)
+
+    map = {}
+
+    for i in range(len(task_id_list)):
+        task_id = task_id_list[i]
+        name=""
+        while task_id > 0:
+            j = task_id%26
+            task_id = task_id // 26
+            name+=chr(ord('A')+j-1)
+        
+        title = getTaskTitle(task_id_list[i])
+        ac_list.append(name)
+        map[str(task_id_list[i])] = [name, title]
+    
+    # print(map)
+
+    for task_id in task_id_list:
+        predecessor_tasks = Dependency.objects.filter(
+            dependent_on_task=task_id).values()
+        dependency_str = ""
+        if len(predecessor_tasks) == 0:
+            dependency_str = "-" 
+        else:
+            for predecessor_task in predecessor_tasks:
+                predecessor_id = predecessor_task['parent_task_id']
+                if predecessor_id in task_id_list:
+                    dependency_str+=chr(ord('A')+predecessor_id-1)
+                
+        pred_list.append(dependency_str)
+        duration_list.append(getTaskDuration(task_id))
+
+    # print("ac_list", ac_list)
+    # print("predlist", pred_list)
+    return map, task_id_list, ac_list, pred_list, duration_list
 
 def getSubTaskList(task_id):
     subtask_list = []
@@ -272,6 +328,180 @@ def getTaskListTimeMap(task_list):
 
     # print(time_map)
     return time_map
+
+
+def generateRandomString(length):
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
+
+
+
+def generateDependencyGraph(ancestry):
+    start = []
+    graph = []
+    atts = []
+    path = []
+    new = []
+    st = ""
+    # data = pd.read_csv("data/data"+str(q)+".csv")
+    data = pd.DataFrame(ancestry)
+    last = data.iloc[-1, 0]
+    last = chr(ord(last)+1)
+    # -------------------------------------------
+    for j in range(len(data)):
+        for k in range(len(data.iloc[j, 1])):
+            if data.iloc[j, 1][k] != '-':
+                new.append(data.iloc[j, 1][k])
+    # -------------------------------------------
+    for j in range(len(data)):
+        if not data.iloc[j, 0] in new:
+            st = st+data.iloc[j, 0]
+    # ------------------------------------------
+    if data.shape[1] == 3:
+        df = pd.DataFrame([[last, st, 0]], columns=["ac", "pr", "du"])
+    else:
+        df = pd.DataFrame([[last, st, 0, 0, 0]], columns=[
+                        "ac", "pr", "b", "m", "a"])
+    data = pd.concat([data, df]) #data.append(df)
+    for i in range(len(data)):
+        graph.append([])
+        atts.append({})
+    for j in range(len(data)):
+        atts[j]["Name"] = data.iloc[j, 0]
+        if data.shape[1] == 3:
+            atts[j]["DU"] = data.iloc[j, 2]
+        else:
+            atts[j]["DU"] = (data.iloc[j, 4] + 4 *
+                            data.iloc[j, 3] + data.iloc[j, 2]) / 6
+        if(data.iloc[j, 1] == "-"):
+            start.append(ord(data.iloc[j, 0])-65)
+            continue
+        for k in range(len(data.iloc[j, 1])):
+            graph[ord(data.iloc[j, 1][k]) -
+                65].append(ord(data.iloc[j, 0])-65)
+
+    level = [None] * (len(graph))
+
+    
+    # BFS
+    s = start.copy()
+    visited = [False] * (len(graph))
+    queue = []
+    for i in s:
+        queue.append(i)
+        level[i] = 0
+        visited[i] = True
+    while queue:
+        s = queue.pop(0)
+        path.append(s)
+        for i in graph[s]:
+            if visited[i] == False:
+                queue.append(i)
+                level[i] = level[s] + 1
+                visited[i] = True
+            else:
+                level[i] = max(level[s]+1, level[i])
+
+    levels = [None] * len(path)
+    for i in range(len(path)):
+        levels[i] = level[path[i]]
+    path = [x for y, x in sorted(zip(levels, path))]
+    # print()
+    # print("Path")
+    for i in path:
+        print(str(chr(i+65)), end=' ')
+    for s in path:
+        # print(str(chr(s+65)), " ", level[s])
+        # -------------Forward--------------------
+        if(data.iloc[s, 1] == "-"):
+            atts[s]["ES"] = 0
+        else:
+            ls = []
+            for k in range(len(data.iloc[s, 1])):
+                ls.append(atts[ord(data.iloc[s, 1][k]) - 65]["EF"])
+            atts[s]["ES"] = max(ls)
+        atts[s]["EF"] = atts[s]["DU"] + atts[s]["ES"]
+        # ---------------------------------
+
+    for i in range(len(graph)):
+        if(graph[i] == []):
+            atts[i]["LF"] = atts[i]["EF"]
+            atts[i]["LS"] = atts[i]["ES"]
+    # print()
+    # print("------------------------")
+    # --------------------backward
+    path.reverse()
+    for i in path:
+        if(data.iloc[i, 1] != "-"):
+            for k in range(len(data.iloc[i, 1])):
+                if "LF" in atts[ord(data.iloc[i, 1][k]) - 65].keys():
+                    atts[ord(data.iloc[i, 1][k]) - 65]["LF"] = min(atts[i]
+                                                                ["LS"], atts[ord(data.iloc[i, 1][k]) - 65]["LF"])
+                else:
+                    atts[ord(data.iloc[i, 1][k]) -
+                        65]["LF"] = atts[i]["LS"]
+                atts[ord(data.iloc[i, 1][k]) - 65]["LS"] = atts[ord(data.iloc[i, 1]
+                                                                    [k]) - 65]["LF"] - atts[ord(data.iloc[i, 1][k]) - 65]["DU"]
+        atts[i]["SK"] = atts[i]["LF"] - atts[i]["EF"]
+    # ----------------------------------------
+    atts[-1]["Name"] = "End"
+    # atts.pop()
+
+
+    ###  print here ###########
+    # for j in range(len(graph)):
+    #     print(atts[j])
+    # print()
+    # ------------------------------------------------
+    G2 = nx.DiGraph()
+
+    for i in range(len(graph)):
+        for j in graph[i]:
+            G2.add_edge(atts[i]["Name"], atts[j]["Name"])
+    temp = []
+    for i in range(len(atts)):
+        temp.append(atts[i]["Name"])
+    temp = dict(zip(temp, atts))
+    nx.set_node_attributes(G2, temp)
+    fig, ax = plt.subplots(figsize=(15, 15))
+    pos = nx.nx_agraph.graphviz_layout(G2, prog='dot')
+    # nx.draw(G2, pos=pos, ax=ax, with_labels=True, font_weight='bold')
+    nx.draw_networkx_edges(G2, pos, edge_color='olive',
+                        width=1, arrowstyle='simple', arrowsize=20, min_source_margin=25, min_target_margin=25)
+    crt = []
+    notcrt = []
+    for j, i in temp.items():
+        if(i["LF"] == i["EF"]):
+            crt.append(j)
+        else:
+            notcrt.append(j)
+    nx.draw_networkx_nodes(G2, pos, node_size=2000,
+                        node_color='seagreen', ax=ax, nodelist=crt)
+    nx.draw_networkx_nodes(G2, pos, node_size=1000,
+                        node_color='wheat', ax=ax, nodelist=notcrt)
+    nx.draw_networkx_labels(G2, pos, ax=ax, font_weight="bold",
+                            font_color="black", font_size=16)
+
+    def without(d, keys={"Name"}):
+        return {x: d[x] for x in d if x not in keys}
+    for node in G2.nodes:
+        xy = pos[node]
+        node_attr = G2.nodes[node]
+        d = G2.nodes[node]
+        d = without(d)
+        text = '\n'.join(f'{k}: {v}' for k,
+                        v in d.items())
+        ax.annotate(text, xy=xy, xytext=(50, 5), textcoords="offset points",
+                    bbox=dict(boxstyle="round", fc="lightgrey"),
+                    arrowprops=dict(arrowstyle="wedge"))
+    ax.axis('off')
+    fig_name = "fig_"+generateRandomString(10)+".png"
+    plt.savefig(MEDIA_ROOT+"/"+ fig_name)
+
+    return fig_name
+
+
 
 # error in implementation
 
